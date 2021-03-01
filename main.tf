47bf0dd45f969f9f93f0166ad75a98533aa0bb2b
Mykhailo Skliar <mykhailo.skliar@c-com.net>
2021-03-01 01:47:14 +0100

    fixed main.tf

--- main.tf
+++ main.tf
@@ -7,17 +7,17 @@ locals {
   cluster_name = "${var.cluster_name}-${var.env_name}"
 }
 
-
 ## AWS VPC definition
 resource "aws_vpc" "main" {
-  cidr_block = var.main_vpc_cidr
+  cidr_block           = var.main_vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
     "Name"                                        = local.vpc_name,
     "kubernetes.io/cluster/${local.cluster_name}" = "shared",
   }
 }
-
-# subnet definition
 
 data "aws_availability_zones" "available" {
   state = "available"
@@ -29,9 +29,7 @@ resource "aws_subnet" "public-subnet-a" {
   availability_zone = data.aws_availability_zones.available.names[0]
 
   tags = {
-    "Name" = (
-      "${local.vpc_name}-public-subnet-a"
-    )
+    "Name"                                        = "${local.vpc_name}-public-subnet-a"
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/elb"                      = "1"
   }
@@ -43,9 +41,7 @@ resource "aws_subnet" "public-subnet-b" {
   availability_zone = data.aws_availability_zones.available.names[1]
 
   tags = {
-    "Name" = (
-      "${local.vpc_name}-public-subnet-b"
-    )
+    "Name"                                        = "${local.vpc_name}-public-subnet-b"
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/elb"                      = "1"
   }
@@ -57,9 +53,7 @@ resource "aws_subnet" "private-subnet-a" {
   availability_zone = data.aws_availability_zones.available.names[0]
 
   tags = {
-    "Name" = (
-      "${local.vpc_name}-private-subnet-a"
-    )
+    "Name"                                        = "${local.vpc_name}-private-subnet-a"
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = "1"
   }
@@ -71,15 +65,12 @@ resource "aws_subnet" "private-subnet-b" {
   availability_zone = data.aws_availability_zones.available.names[1]
 
   tags = {
-    "Name" = (
-      "${local.vpc_name}-private-subnet-b"
-    )
+    "Name"                                        = "${local.vpc_name}-private-subnet-b"
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = "1"
   }
 }
 
-# Internet gateway and routing tables for public subnets
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
@@ -110,7 +101,6 @@ resource "aws_route_table_association" "public-b-association" {
   subnet_id      = aws_subnet.public-subnet-b.id
   route_table_id = aws_route_table.public-route.id
 }
-
 
 resource "aws_eip" "nat-a" {
   vpc = true
@@ -146,9 +136,9 @@ resource "aws_nat_gateway" "nat-gw-b" {
   }
 }
 
-
 resource "aws_route_table" "private-route-a" {
   vpc_id = aws_vpc.main.id
+
   route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat-gw-a.id
@@ -161,6 +151,7 @@ resource "aws_route_table" "private-route-a" {
 
 resource "aws_route_table" "private-route-b" {
   vpc_id = aws_vpc.main.id
+
   route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat-gw-b.id
@@ -179,4 +170,17 @@ resource "aws_route_table_association" "private-a-association" {
 resource "aws_route_table_association" "private-b-association" {
   subnet_id      = aws_subnet.private-subnet-b.id
   route_table_id = aws_route_table.private-route-b.id
+}
+
+# Create a Route 53 zone for DNS support inside the VPC
+resource "aws_route53_zone" "private-zone" {
+  # AWS requires a lowercase name. 
+  #name = "lower(${var.env_name}.${var.vpc_name}.com)"
+  name = "${var.env_name}.${var.vpc_name}.com"
+  #name = "testing.com"
+  force_destroy = true
+
+  vpc {
+    vpc_id = aws_vpc.main.id
+  }
 }
